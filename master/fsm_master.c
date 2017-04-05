26eefb66acb94bbcb963d4895841e519558ad7dd
Graeme Foot <Graeme.Foot@touchcut.com>
2017-04-05 14:39:54 +1200

    Allow the application to explicitly process slave requests cyclically from the applications context.  ecrt_master_exec_slave_requests() can take some time to return so should be called from a separate thread from you realtime application.
    For example (in RTAI) create a soft rt thread that loops, waiting on the main
    RT thread to complete its cycle before calling ecrt_master_exec_slave_requests()

--- master/fsm_master.c
+++ master/fsm_master.c
@@ -292,6 +292,7 @@ void ec_fsm_master_state_broadcast(
                 "link down on %s device. Clearing slave list.\n",
                 ec_device_names[fsm->dev_idx != 0]);
 
+        ec_master_slaves_not_available(master);
 #ifdef EC_EOE
         ec_master_eoe_stop(master);
         ec_master_clear_eoe_handlers(master);
@@ -351,6 +352,7 @@ void ec_fsm_master_state_broadcast(
             fsm->idle = 0;
             fsm->scan_jiffies = jiffies;
 
+            ec_master_slaves_not_available(master);
 #ifdef EC_EOE
             ec_master_eoe_stop(master);
             ec_master_clear_eoe_handlers(master);
@@ -409,6 +411,7 @@ void ec_fsm_master_state_broadcast(
             master->slave_count = count;
             master->fsm_slave = master->slaves;
 
+            ec_master_slaves_available(master);
             ec_fsm_master_enter_dc_read_old_times(fsm);
             return;
         }
